693bfe169eb3ca8d0761e430ed99741ac662f469
Roberto Mansfield <robertom@sas.upenn.edu>
2014-08-13 15:07:04 -0400

    Fix GoogleOAuth2TokenExtension after bundle rename

--- DependencyInjection/GoogleOAuth2TokenExtension.php
+++ DependencyInjection/GoogleOAuth2TokenExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class GmailOAuth2TokenExtension extends Extension
+class GoogleOAuth2TokenExtension extends Extension
 {
     /**
      * {@inheritDoc}
